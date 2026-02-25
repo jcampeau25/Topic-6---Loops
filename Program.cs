71fe3a7ccbb1817ace4ff41fea597f5ec0ecfa2f
Joshua Campeau <339729675@GoTVDSB.ca>
2026-02-25 13:14:55 -0500

    start bill payment

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Topic_6___Loops
+﻿using System.ComponentModel.DataAnnotations;
+using System.Resources;
+
+namespace Topic_6___Loops
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            // Part 1
+
             int max, min, num;
             min = 0;
 
@@ -31,12 +37,104 @@
             }
 
             Console.WriteLine($"Great! Your minimum was {min} your maximum number was {max} and the number you chose was {num}");
+            Console.WriteLine();
+
+            // Part 2
+
+
+            double balance = 150;
+            string response;
+            double deposit, withdrawal;
+
+            bool done = false;
+
+            while (!done)
+            {
+
+
+                Console.WriteLine("Welcome to Bank Of Blorb!");
+                Console.WriteLine("Please enter one of the following options");
+                Console.WriteLine("1. View Balance");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdrawal");
+                Console.WriteLine("4. Bill payment");
+                Console.WriteLine("5. Quit");
+
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "1" || response == "VIEW BALANCE")
+                {
+
+                    Console.WriteLine("$0.75 transaction fee deducted");
+                    balance = balance - 0.75;
+                    Console.WriteLine($"Your current balance is {balance}");
+                }
+
+                if (response == "2" || response == "DEPOSIT")
+                {
+
+                    Console.WriteLine("$0.75 transaction fee deducted");
+                    balance = balance - 0.75;
+
+
+                    Console.WriteLine($"How much would you like to deposit?");
+                    while (!double.TryParse(Console.ReadLine(), out deposit) || deposit < 0)
+                    {
+                        Console.WriteLine("Invalid input! Please enter a valid positive numerical input");
+                        balance = balance - 0.75;
+                        Console.WriteLine("$0.75 transaction fee deducted");
+                        Console.WriteLine("Please try again");
+
+                    }
+
+                    balance = balance - 0.75;
+                    balance = balance + deposit;
+                    Console.WriteLine($"You deposited {deposit.ToString("C")}. Your new balance is {balance.ToString("C")}");
+
+
+                }
+
+
+                if (response == "3" || response == "WITHDRAWAL")
+                {
+                    Console.WriteLine("$0.75 transaction fee deducted");
+                    balance = balance - 0.75;
+
+                    Console.WriteLine("How much would you like to withdrawal?");
+                    while (!double.TryParse(Console.ReadLine(), out withdrawal) || withdrawal > balance)
+                    {
+                        Console.WriteLine("Invalid input! You either entered an invalid input or have insufficient funds to withdrawal");
+                        balance = balance - 0.75;
+                        Console.WriteLine("$0.75 transaction fee deducted");
+                        Console.WriteLine("Please try again");
+
+                    }
+
+       
+
+                    if (balance >= withdrawal)
+                    {
+
+                        balance = balance - withdrawal;
+                        Console.WriteLine($"You withdrew {withdrawal.ToString("C")}. Your new balance is {balance.ToString("C")}");
+
+
+
+                    }
+
+
+                    if (response == "4" || response == "BILL PAYMENT")
+                    {
+
+                    }
 
 
 
 
+                }
 
-
+            }
 
 
 
